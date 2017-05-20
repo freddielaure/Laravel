0da46c5bab432a9d2a297a10024d619dbaedecb7
Laure Tchagam <freddielaure@Laures-MacBook-Air.local>
2017-05-20 11:13:03 +0200

    Ich habe die Homepage implementiert # ------------------- ----- >8 ------------------------ # Do not touch the line above. # Everything below will be removed. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # Changes to be committed: # # 	new file:   resources/views/home.blade.php # # Changes not staged for commit: # # # 	modified:   .DS_Store # 	modified:   app/.DS_Store # 	modified:   app/Http/.DS_Store # 	modified:   app/Http/Controllers/.DS_Store # 	modified:   app/Http/Controllers/Auth/RegisterController.php # 	modified:   app/Http/routes.php # 	modified:   app/Providers/AppServiceProvider.php # 	modified:   app/User.php # 	modified:   bootstrap/.DS_Store # 	modified:   config/app.php # 	modified:   config/database.php # 	modified:   public/.DS_Store # 	modified:   public/bootstrap/.DS_Store # 	modified:   public/bootstrap/dist/.DS_Store # 	modified:   resources/.DS_Store # 	modified:   resources/views/.DS_Store # 	modified:   resources/views/app.blade.php # 	modified:   resources/views/test.blade.php # 	modified:   resources/views/welcome.blade.php # 	modified:   routes/web.php # 	modified:   storage/.DS_Store # 	deleted:    testtt.php # # Untracked files: # # 	app/Http/Controllers/HomeController.php # 	public/css/home_style.css # 	resources/views/auth/passwords/ # 	resources/views/auth/register.blade.php # 	resources/views/layouts/

---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container ID">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+
+
+        </div>
+    </div>
+</div>
+
+
+<div class="container wrt">
+<div class="row">
+  <div class="col-sm-4">
+    <div class="card card1">
+    <div class="t-Card-icon u-color1"><span class="t-Icon fa fa-line-chart"><span class="t-Card-initials" role="presentation"></span></span></div>
+      <div class="card-block">
+        <h3 class="card-title">Special title treatment</h3>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card card1">
+      <div class="t-Card-icon u-color2"><span class="t-Icon fa fa-line-chart"><span class="t-Card-initials" role="presentation"></span></span></div>
+      <div class="card-block">
+        <h3 class="card-title">Special title treatment</h3>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-4">
+    <div class="card card3">
+      <div class="t-Card-icon u-color3"><span class="t-Icon fa fa-line-chart"><span class="t-Card-initials" role="presentation"></span></span></div>
+      <div class="card-block">
+        <h3 class="card-title">Special title treatment</h3>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+@endsection
+<link href="/css/home_style.css" rel="stylesheet">
